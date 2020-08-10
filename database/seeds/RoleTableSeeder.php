a3e2bc343efc46b008d60c0b3d8bcf556c46945b
thetpainghtut <thetpainghtut.bf@gmail.com>
2020-08-10 09:34:51 +0630

    edit roletableseeder

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -13,11 +13,13 @@ class RoleTableSeeder extends Seeder
     public function run()
     {
         DB::table('roles')->insert([
-            'name' => 'customer'
+            'name' => 'customer',
+            'guard_name' => 'web'
         ]);
 
         DB::table('roles')->insert([
-            'name' => 'admin'
+            'name' => 'admin',
+            'guard_name' => 'web'
         ]);
     }
 }
